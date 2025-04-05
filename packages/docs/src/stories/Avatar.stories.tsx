f301dc62dd460610c3ea45c1e1195980ad5242c5
Celio Junior <celioju@hotmail.com>
2025-04-05 16:36:05 -0300

    Ajustes

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/aiemao.png',
     alt: 'Celio Junior',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
